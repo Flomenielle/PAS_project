bbf1c0ad50a63a8f281018d92b110439432c0306
Floflo <florian.menielle@outlook.fr>
2021-12-01 23:03:19 +0100

    Initial commit

--- driver/code/hand_coded_lane_follower.py
+++ driver/code/hand_coded_lane_follower.py
@@ -310,14 +310,14 @@ def test_photo(file):
 
 def test_video(video_file):
     lane_follower = HandCodedLaneFollower()
-    cap = cv2.VideoCapture(video_file + '.avi')
+    cap = cv2.VideoCapture(video_file)
 
     # skip first second of video.
     for i in range(3):
         _, frame = cap.read()
 
     video_type = cv2.VideoWriter_fourcc(*'XVID')
-    video_overlay = cv2.VideoWriter("%s_overlay.avi" % (video_file), video_type, 20.0, (320, 240))
+    video_overlay = cv2.VideoWriter("vid_circuit_overlay.avi", video_type, 20.0, (320, 568))
     try:
         i = 0
         while cap.isOpened():
@@ -325,9 +325,9 @@ def test_video(video_file):
             print('frame %s' % i )
             combo_image= lane_follower.follow_lane(frame)
             
-            cv2.imwrite("%s_%03d_%03d.png" % (video_file, i, lane_follower.curr_steering_angle), frame)
+            #cv2.imwrite("%s_%03d_%03d.png" % (video_file, i, lane_follower.curr_steering_angle), frame)
             
-            cv2.imwrite("%s_overlay_%03d.png" % (video_file, i), combo_image)
+            #cv2.imwrite("%s_overlay_%03d.png" % (video_file, i), combo_image)
             video_overlay.write(combo_image)
             cv2.imshow("Road with Lane line", combo_image)
 
@@ -343,7 +343,7 @@ def test_video(video_file):
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
-    test_video('/home/pi/DeepPiCar/driver/data/tmp/video01')
+    test_video(r".\DeepPiCar\driver\data\vid-circuit-x06.avi")
     #test_photo('/home/pi/DeepPiCar/driver/data/video/car_video_190427_110320_073.png')
     #test_photo(sys.argv[1])
     #test_video(sys.argv[1])
